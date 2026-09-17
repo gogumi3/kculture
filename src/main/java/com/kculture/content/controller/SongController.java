e38f41e60f77a3bdfffa9f15ef97771d93ff9f4e
gogumi3 <nosanghky0418@naver.com>
2026-09-17 11:45:51 +0900

    Fix song search endpoint

--- src/main/java/com/kculture/content/controller/SongController.java
+++ src/main/java/com/kculture/content/controller/SongController.java
@@ -24,6 +24,7 @@ public class SongController {
         return songService.findSong(id);
     }
 
+    @GetMapping("/search")
     public List<SongResponse> searchSongs(
             @RequestParam String keyword
     ) {
